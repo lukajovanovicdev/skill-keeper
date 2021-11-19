43d83221beadaeae357f04894410a336b2582ce2
Nevena Gavrilov <Nevena.Gavrilov@bosch.com>
2021-11-19 13:47:01 +0100

    Styles changes

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,14 +1,23 @@
+import { Button } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Grid from '@mui/material/Grid';
+
 import React from "react";
 
 import classes from "./Skill.module.css";
+import { Box } from "@mui/system";
 
 export default function Skill({ id, name, url }) {
   return (
-    <div className={classes.Card}>
-      <div>
-        <img src={url}></img>
-      </div>
-      <div>{name}</div>
-    </div>
+    <Box sx={{ flexGrow: 1 }} className={classes.Card}>
+      <Grid container justifyContent="space-between">
+        <Grid item>
+          <img src={url}></img>
+        </Grid>
+        <Grid item>{name}</Grid>
+        <Grid item><Button variant="outlined" startIcon={<DeleteIcon />}>Delete</Button></Grid>
+      </Grid>
+    </Box>
+    
   );
 }
