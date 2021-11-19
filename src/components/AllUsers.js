295d62a077aff98933f937a14784fdd92d37c402
Nevena Gavrilov <Nevena.Gavrilov@bosch.com>
2021-11-19 16:48:58 +0100

    project upadte

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -1,9 +1,9 @@
 import React from "react";
 import User from "./User";
-
+import "./User.css";
 export default function AllUsers({ users }) {
   return (
-    <div>
+    <div className="usersStyle">
       {users.map((user) => (
         <User
           name={user.name}
